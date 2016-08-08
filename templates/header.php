aaab7568b34e7720ff28812afcb59e15fd7c4e85
Ilincescu Mihai <mihai@ilincescu.ro>
2016-08-08 22:57:37 +0300

    fixed top collapsing menu

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,20 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
-      endif;
-      ?>
+    <nav class="nav-primary navbar-fixed-top">
+      <div class="container">
+         <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapsingNavbar" aria-controls="collapsingNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            &#9776;
+          </button>
+          <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation','container' => 'div', 
+                          'container_class'   => 'collapse navbar-toggleable-xs', 'container_id'=> 'collapsingNavbar',
+                          'menu_class' => 'nav navbar-nav']);
+          endif;
+          ?>
+        </div>
+      </div>
     </nav>
   </div>
 </header>
